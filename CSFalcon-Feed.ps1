d358c61f7c77c4ab5c4f0e7358b52ba3f1ea3f3e
MEHMET VATANSEVER <31635986+mehmetvatansever@users.noreply.github.com>
2023-02-10 06:35:00 +0300

    Update and rename sophos.yml to CSFalcon-Feed.ps1

---
+++ CSFalcon-Feed.ps1
@@ -0,0 +1,16 @@
+####__CrowdStrike Indicator Feed from Powershell Script__####
+#
+Invoke-WebRequest http://ip-or-domain-url/data.txt -OutFile C:\Windows\Temp\misp-ipv4.csv
+#
+Request-FalconToken -ClientId !entry_clientid! -ClientSecret !entry_clientsecret!
+#
+$dateFormat='yyyy-MM-ddTHH:mm:00Z'
+#
+$90DaysAgo = (Get-Date).AddDays(+90)
+#
+$Date = Get-Date -Date $90DaysAgo -Format $dateFormat
+#
+$misp = import-csv C:\Windows\Temp\misp-ipv4.csv -Header IP
+#
+foreach($misps in $misp){New-FalconIoc -Source "1" -Action "Detect" -Expiration $Date -Description "MISP FEED IP LIST" -Type ipv4 -Value $misps.ip -Platform "Windows" -Severity "Critical" -AppliedGlobally $true}
+#
